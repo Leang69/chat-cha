b8328f832183072910075b76b31f5671acd81aea
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-06-29 01:37:09 +0700

    commit

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -2,72 +2,59 @@ import React from "react";
 import './../style/login.scss'
 import google from "./../img/googlelogo.png"
 import {Link} from 'react-router-dom'
+import {useForm} from "react-hook-form";
 
 export default function Login() {
-  return (
-    <div>
-      <div
-        class="
-        container-fluid
-        my-5
-        p-3
-        col-lg-6 col-sm-8 col-xs-8 col-10
-        d-flex
-        flex-column
-        align-items-center
-      "
-      >
-        <h1 class="title">Chat Cha</h1>
-        <div class="formLogin">
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1 text-s" class="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <p class="mb-3 text-primary text-end">Forget Password</p>
-          <button class="d-flex mb-3 justify-content-center btn btn-primary w-100 m-auto">
-            Login
-          </button>
-          <button
-            class="
-            d-flex
-            mb-3
-            justify-content-center
-            btn btn-primary
-            w-100
-            login_google
-          "
-          >
-            <img class="me-3" src={google} />
-            Login with Google
-          </button>
-          <p class="mb-3 text-center fw-bolder">
-            Don't have an account? <span class="text-primary"><Link to="signup">Sign Up</Link></span>
-          </p>
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const loginHandle = (data) => {
+        console.log(data)
+    }
+    return (
+        <div>
+            <div
+                className="container-fluid my-5 p-3 col-lg-6 col-sm-8 col-xs-8 col-10 d-flex flex-column align-items-center">
+                <h1 className="title">Chat Cha</h1>
+                <form onSubmit={handleSubmit(loginHandle)} className="formLogin">
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            {...register('email',{required:true})}
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                        />
+                        {errors.email?.type === 'required' && <p className="text-danger">Email is require</p>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1 text-s" className="form-label">
+                            Password
+                        </label>
+                        <input
+                            {...register('password',{required: true,minLength:8})}
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                        />
+                        {errors.password?.type === 'required' &&  <p className="text-danger">Password is require</p>}
+                        {errors.password?.type === 'minLength' && <p className="text-danger">Password minimum 8 digit</p>}
+                    </div>
+                    <p className="mb-3 text-primary text-end">Forget Password</p>
+                    <button type="submit" className="d-flex mb-3 justify-content-center btn btn-primary w-100 m-auto">
+                        Login
+                    </button>
+                    <button className="d-flex mb-3 justify-content-center btn btn-primary w-100 login_google">
+                        <img className="me-3" src={google}/>
+                        Login with Google
+                    </button>
+                    <p className="mb-3 text-center fw-bolder">
+                        Don't have an account? <span className="text-primary"><Link to="signup">Sign Up</Link></span>
+                    </p>
+                </form>
+            </div>
         </div>
-      </div>
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"
-      ></script>
-    </div>
-  );
+    );
 }
 
--- src/component/signup.jsx
+++ src/component/signup.jsx
@@ -2,84 +2,79 @@ import React from "react";
 import './../style/signUp.scss'
 import google from "./../img/googlelogo.png"
 import {Link} from 'react-router-dom'
+import {useForm} from "react-hook-form";
 
 export default function SignUp() {
-  return (
-    <div
-      class="
-        container-fluid
-        my-5
-        p-3
-        col-lg-6 col-sm-8 col-xs-8 col-10
-        d-flex
-        flex-column
-        align-items-center
-      "
-    >
-      <h1 class="title">Chat Cha</h1>
-      <div class="formSignUp">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const signUphandle = (data) => {
+        console.log(data)
+    }
+    return (
+        <div className="container-fluid my-5 p-3 col-lg-6 col-sm-8 col-xs-8 col-10 d-flex flex-column align-items-center">
+            <h1 className="title">Chat Cha</h1>
+            <form onSubmit={handleSubmit(signUphandle)} className="formSignUp">
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">
+                        Email address
+                    </label>
+                    <input
+                        {...register('email', {required:true})}
+                        type="email"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                    />
+                    {errors.email?.type === 'required' && <p className="text-danger">Email is require</p>}
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">
+                        Username
+                    </label>
+                    <input
+                        {...register('username',{required:true})}
+                        type="text"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                    />
+                    {errors.username?.type === 'required' && <p className="text-danger">Username is require</p>}
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                        Password
+                    </label>
+                    <input
+                        {...register('password',{required:true,minLength: 8})}
+                        type="password"
+                        className="form-control"
+                    />
+                    {errors.password?.type === 'required' && <p className="text-danger">Password is require</p>}
+                    {errors.password?.type === 'minLength' && <p className="text-danger">Password minimum 8 digit</p>}
+
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">
+                        Confirm Password
+                    </label>
+                    <input
+                        {...register('confirm_password',{required:true,minLength: 8})}
+                        type="password"
+                        className="form-control"
+                    />
+                    {errors.confirm_password?.type === 'required' && <p className="text-danger">Confirm Password is require</p>}
+                    {errors.confirm_password?.type === 'minLength' && <p className="text-danger">Confirm Password minimum 8digit</p>}
+                </div>
+                <button type="submit" className="d-flex mb-3 justify-content-center btn btn-primary w-100">
+                    Sign Up
+                </button>
+                <button className="d-flex mb-3 justify-content-center btn btn-primary w-100 signup_google " >
+                    <img className="me-3" src={google}/>
+                    Sign Up with Google
+                </button>
+                <p className="mb-3 text-center fw-bolder">
+                    Have an account? <span className="text-primary"><Link to="login">Login</Link></span>
+                </p>
+            </form>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <button class="d-flex mb-3 justify-content-center btn btn-primary w-100">
-          Sign Up
-        </button>
-        <button
-          class="
-            d-flex
-            mb-3
-            justify-content-center
-            btn btn-primary
-            w-100
-            signup_google
-          "
-        >
-          <img class="me-3" src={google} />
-          Sign Up with Google
-        </button>
-        <p class="mb-3 text-center fw-bolder">
-          Have an account? <span class="text-primary"><Link to="login">Login</Link></span>
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
